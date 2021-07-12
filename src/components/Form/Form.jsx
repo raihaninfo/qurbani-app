7865bf3efe5341c4b4c68654e6d471bd613924fc
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-12 16:21:38 +0600

    code format

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -9,6 +9,7 @@ const Form = () => {
                     <label for="name" className="form-label">আপনার নাম লিখুন</label>
                     <input className="form-control" type="text" name="" id="name" />
                 </div>
+                
                 <div className='mb-3'>
                     <label for="amount" className="form-label">আপনার কত টাকা আছে ?</label>
                     <input className="form-control" type="number" name="" id="amount" />
