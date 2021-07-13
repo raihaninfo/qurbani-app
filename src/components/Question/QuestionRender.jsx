975c3fe35fdb230c2d1e202ef73a2d49e30661e9
raihaninfo <raihanmahmudi35@gmail.com>
2021-07-13 14:04:21 +0600

    code format

--- src/components/Question/QuestionRender.jsx
+++ src/components/Question/QuestionRender.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import allData from '../../allData';
-import Question from './Question';
+import React, { useState } from "react";
+import allData from "../../allData";
+import Question from "./Question";
 
 const QuestionRender = () => {
-    const allQuestion = allData.slice(0, 30);
-    const [question] = useState(allQuestion);
-    return (
-        <div>
-            <div className="header my-4">
-                <h2>কুরবানি নিয়ে কিছু প্রশ্ন উত্তর</h2>
-                <p>প্রশ্নগুলো মাসিক আল-কাওসার থেকে সংগ্রহ করা</p>
-            </div>
-                {question.map((question)=>(
-                <Question question={question} key={question.key}></Question>
-            ))}
-        </div>
-    );
+  const allQuestion = allData.slice(0, 30);
+  const [question] = useState(allQuestion);
+  return (
+    <div>
+      <div className="header my-4">
+        <h2>কুরবানি নিয়ে কিছু প্রশ্ন উত্তর</h2>
+        <p>প্রশ্নগুলো মাসিক আল-কাওসার থেকে সংগ্রহ করা</p>
+      </div>
+      {question.map((question) => (
+        <Question question={question} key={question.key}></Question>
+      ))}
+    </div>
+  );
 };
 
 export default QuestionRender;
--- src/components/QurbaniDetails/HistoryOfQurbani.jsx
+++ src/components/QurbaniDetails/HistoryOfQurbani.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const HistoryOfQurbani = () => {
-    return (
-        <div>
-            <h4>History of Qurbani</h4>
-        </div>
-    );
+  return (
+    <div>
+      <h4>History of Qurbani</h4>
+    </div>
+  );
 };
 
 export default HistoryOfQurbani;
