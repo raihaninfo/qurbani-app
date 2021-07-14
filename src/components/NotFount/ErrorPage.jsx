8b8ad55eaca14fbae54752b6f1203741a19bf485
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-07-14 16:44:45 +0600

    404 page change text

--- src/components/NotFount/ErrorPage.jsx
+++ src/components/NotFount/ErrorPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ErrorPage.css";
 
 const ErrorPage = () => {
   return (
     <div className="not-fount">
       <div className="center-text">
-        <h5 className="header-error">404 || Not Fount</h5>
+        <h5 className="header-error py-2">404 || Not Fount</h5>
         <p className="error-pra">
-          Sorry, but the page you were trying to view does not exist
+        দুঃখিত, আপনি যে পাতাটি খুঁজছেন, তা পাওয়া যাইনি। <Link to='/'>মূল পাতাতে</Link> ফিরে যান।
         </p>
       </div>
     </div>
