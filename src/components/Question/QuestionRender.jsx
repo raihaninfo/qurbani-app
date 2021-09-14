ab9bcee1d7269ab3dbad2eba2760665abe2fc1df
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-14 21:13:37 +0600

    lkfj

--- src/components/Question/QuestionRender.jsx
+++ src/components/Question/QuestionRender.jsx
@@ -9,7 +9,7 @@ const QuestionRender = () => {
     <div className='container'>
       <div className="header py-4">
         <h2 className='py-1'>কুরবানি নিয়ে কিছু প্রশ্ন উত্তর</h2>
-        <p className='border-bottom'>প্রশ্নগুলো মাসিক আল-কাওসার থেকে সংগ্রহ করা</p>
+        <p className='border-bottom pb-1'>প্রশ্নগুলো মাসিক আল-কাওসার থেকে সংগ্রহ করা</p>
       </div>
       {question.map((question) => (
         <Question question={question} key={question.key}></Question>
