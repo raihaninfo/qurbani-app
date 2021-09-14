3e77fdf019b0194917139479bbda53db52a8b445
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-14 21:23:10 +0600

    add sum text

--- src/components/NotFount/ErrorPage.jsx
+++ src/components/NotFount/ErrorPage.jsx
@@ -8,8 +8,9 @@ const ErrorPage = () => {
       <div className="center-text">
         <h5 className="header-error py-2">404 || Not Fount</h5>
         <p className="error-pra">
-        দুঃখিত! আপনি যে পাতাটি খুঁজছেন, তা পাওয়া যাইনি। <Link to='/'>মূল পাতাতে</Link> ফিরে যান।
+        দুঃখিত! আপনি যে পাতাটি খুঁজছেন, তা পাওয়া যাইনি। <Link to='/'>মূল পাতাতে</Link> ফিরে যান। 
         </p>
+        <p>ধন্যবাদ</p>
       </div>
     </div>
   );
