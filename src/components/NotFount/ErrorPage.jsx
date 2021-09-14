1a510815ec747b22f9714252f4a927cb573bd3f6
Md Abu. Raihan <66956067+raihaninfo@users.noreply.github.com>
2021-09-14 20:46:29 +0600

    edit 404 page

--- src/components/NotFount/ErrorPage.jsx
+++ src/components/NotFount/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
       <div className="center-text">
         <h5 className="header-error py-2">404 || Not Fount</h5>
         <p className="error-pra">
-        দুঃখিত, আপনি যে পাতাটি খুঁজছেন, তা পাওয়া যাইনি। <Link to='/'>মূল পাতাতে</Link> ফিরে যান।
+        দুঃখিত! আপনি যে পাতাটি খুঁজছেন, তা পাওয়া যাইনি। <Link to='/'>মূল পাতাতে</Link> ফিরে যান।
         </p>
       </div>
     </div>
